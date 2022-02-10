970d313e066ae0039302300b10ef7fe3988162a1
hinal.patel <hinal.patel@1R-79>
2022-02-10 17:54:08 +0530

    Add project files.

---
+++ MainActivity.cs
@@ -0,0 +1,48 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+using Xamarin.Essentials;
+
+namespace VibrateDemo
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private Button button;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            button = FindViewById<Button>(Resource.Id.button1);
+            button.Click += Button_Click;
+
+        }
+        private void Button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Use default vibration length
+                Vibration.Vibrate();
+
+                // Or use specified time
+                var duration = TimeSpan.FromSeconds(1);
+                Vibration.Vibrate(duration);
+                Toast.MakeText(this, "VIBRATION ", ToastLength.Short).Show();
+            }
+            catch (FeatureNotSupportedException ex)
+            {
+                Log.Debug("hjhhg", ex.Message);
+            }
+            catch (Exception ex)
+            {
+                // Other error has occurred.
+            }
+        }
+    }
+}
